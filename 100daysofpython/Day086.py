b7405c4d1cfe8259fed541659669689fd728b4a8
Ingryd Lorena <ingrydlorena766@gmail.com>
2025-02-25 16:30:10 -0300

    testing

---
+++ 100daysofpython/Day086.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIP.OUT)
+
+try:
+    while True:
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
