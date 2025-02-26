09695d39dceeaad3fc2acdbd7c48113468752c12
Ingryd Lorena <ingrydlorena766@gmail.com>
2025-02-26 09:26:46 -0300

    adding day 86 and 87

--- 100daysofpython/Day086.py
+++ 100daysofpython/Day086.py
@@ -1,17 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
+# Set up the GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIP.OUT)
+
+# Set the pin where the LED is connected
+LED_PIN = 17
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(18, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(18, GPIO.LOW)
-        time.sleep(1)
-
+        # Turn the LED on
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)  # Wait for 1 second
+        # Turn the LED off
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)  # Wait for 1 second
 except KeyboardInterrupt:
-    pass
+    pass  # Exit the program gracefully when Ctrl+C is pressed
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup()  # Reset all GPIO settings
+
+# i don't have material to do this task, so the AI returned what do you have to do
